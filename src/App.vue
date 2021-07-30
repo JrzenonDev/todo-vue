6379d9f75326191207836a9d663f4f8e3007d895
JrzenonDev <jrobertoonb@gmail.com>
2021-07-29 23:40:00 -0300

    configure framework spectre css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="container grid-xs py-2">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <h1>Hello World</h1>
+    </div>
   </div>
 </template>
 
@@ -12,14 +15,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

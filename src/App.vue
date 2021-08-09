2b1e29f0efd13237f59021c7b3c1f7e86187202e
JrzenonDev <jrobertoonb@gmail.com>
2021-08-08 22:57:02 -0300

    navigation router-link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div class id="app">
-    <router-view></router-view>
+    <div class="container grid-lg">
+      <header class="navbar">
+        <section class="navbar-section">
+          <router-link class="btn btn-link" to="/">Home</router-link>
+          <router-link class="btn btn-link" to="/kanban">Kanban</router-link>
+        </section>
+        <section class="navbar-center">
+          <img class="img-responsive img-logo" src="@/assets/logo.png" alt="Todo Vue JS">
+        </section>
+        <section class="navbar-section">
+          <a href="https://github.com/JrzenonDev/" class="btn btn-link" target="_blank">GitHub</a>
+        </section>
+      </header>
+      <router-view class="pt-2" />
+    </div>
   </div>
 </template>

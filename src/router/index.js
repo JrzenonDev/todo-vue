4242cf4cff0ec02b2528ef202576fcc71ab85517
JrzenonDev <jrobertoonb@gmail.com>
2021-08-08 22:58:13 -0300

    register new route kanban

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home.vue'
+import Kanban from '@/views/Kanban/Kanban.vue'
 
 Vue.use(Router)
 
 const routes = [
   {
-    name: 'Home',
+    name: 'home',
     path: '/',
     component: Home
+  },
+  {
+    name: 'kanban',
+    path: '/kanban',
+    component: Kanban
   }
 ]
 

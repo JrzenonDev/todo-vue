6e0286090a25c95630318f66bd83c13a047afcbe
JrzenonDev <jrobertoonb@gmail.com>
2021-08-09 22:12:46 -0300

    init store

--- src/store.js
+++ src/store.js
@@ -3,7 +3,29 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const state = { list: [] }
+
+const actions = {
+  addTodo ({ commit }, todo) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        todo.id = Date.now()
+        commit('addTodo', todo)
+      }, 1000)
+    })
+  }
+}
+
+const mutations = {
+  addTodo (state, payload) {
+    state.list.push(payload)
+  }
+}
+
 const store = new Vuex.Store({
+  state,
+  actions,
+  mutations
 })
 
 export default store

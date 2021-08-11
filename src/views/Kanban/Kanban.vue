3fe0a9d2b9ddc2df6ea733ea200ec276563ea780
JrzenonDev <jrobertoonb@gmail.com>
2021-08-11 01:36:29 -0300

    create layout in component Kanban

--- src/views/Kanban/Kanban.vue
+++ src/views/Kanban/Kanban.vue
@@ -1,14 +1,55 @@
 <template>
-  <main>
-    <h1>
-      Board
-    </h1>
-  </main>
+  <div class="container-board">
+    <div class="columns">
+      <div class="column col-5">
+        <div class="panel">
+          <div class="panel-header">
+            <div class="panel-title">Todo</div>
+          </div>
+          <div class="panel-body">
+            <!-- contents -->
+          </div>
+          <div class="panel-footer">
+            <!-- buttons or inputs -->
+          </div>
+        </div>
+      </div>
+      <div class="column col-5">
+        <div class="panel">
+          <div class="panel-header">
+            <div class="panel-title">Done</div>
+          </div>
+          <div class="panel-body">
+            <!-- contents -->
+          </div>
+          <div class="panel-footer">
+            <!-- buttons or inputs -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'Kanban'
 }
 </script>
+
+<style scoped>
+.container-board {
+  height: calc(100vh - 200px);
+  padding: 10px;
+}
+
+.columns {
+  height: 100%;
+}
+
+.columns .column .panel {
+  height: 100%;
+  border: 0;
+  box-shadow: 0 0.25rem 1rem rgba(48, 55, 66, 0.15);
+}
+</style>
